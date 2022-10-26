dafbc3ca0a8a69685ab2d5420579d4353e35eaa3
Vincenzo Nardelli <vincnardelli@gmail.com>
2022-10-26 17:13:22 +0200

    add lezione 6

---
+++ materiale_lezioni/6/taxi_1.R
@@ -0,0 +1,30 @@
+tariffa_min <- 28/60
+tariffa_km <- 1.14
+distanza <- c(1, 0.3, 0.5, 0.8, 0.2)
+
+costo <- 0
+
+for(i in 1:5){
+  velocita <- distanza[i]*60
+  if(velocita < 20){
+    costo <- costo+tariffa_min
+  }else{
+    costo <- costo+tariffa_km*distanza[i]
+  }
+  print(costo)
+}
+costo
+
+
+
+# Alternativa 2
+costo <- 0
+velocita <- distanza*60
+for(i in 1:5){
+  if(velocita[i] < 20){
+    costo <- costo+tariffa_min
+  }else{
+    costo <- costo+tariffa_km*distanza[i]
+  }
+}
+costo
