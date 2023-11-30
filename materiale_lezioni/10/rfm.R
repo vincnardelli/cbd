d1f214e03ec17082879c05369499578add25dba9
Vincenzo Nardelli <vincnardelli@gmail.com>
2023-11-30 21:27:07 +0100

    Add progetto and lezione 10

--- materiale_lezioni/10/rfm.R
+++ materiale_lezioni/10/rfm.R
@@ -1,86 +1,27 @@
 library(dplyr)
 library(lubridate)
-library(ggplot2)
-online_retail <- read.csv("data/online_retail_rfm.csv")
 
-str(online_retail)
-online_retail <- online_retail %>% 
+data <- read.csv("data/online_retail_rfm.csv")
+data <- data %>% 
   mutate(InvoiceDate = as_date(InvoiceDate))
+summary(data)
 
-ggplot(online_retail) +
-  geom_bar(aes(InvoiceDate))
-
-online_retail %>% 
-  group_by(InvoiceDate) %>% 
-  summarise(n=n()) %>% 
-  ggplot() +
-  geom_line(aes(InvoiceDate, n))
-
-
-
-online_retail %>% 
-  mutate(month = floor_date(InvoiceDate, 'month')) %>% 
-  group_by(month) %>% 
-  summarise(n=n()) %>% 
-  ggplot() +
-  geom_line(aes(month, n))
-
-online_retail %>% 
-  mutate(month = floor_date(InvoiceDate, 'month')) %>% 
-  group_by(month) %>% 
-  summarise(n=n()) %>% 
-  ggplot() +
-  geom_col(aes(month, n))
-
-
-rfm <- online_retail %>% 
+rfm <- data %>% 
+  mutate(Recency = as.numeric(today()- InvoiceDate)) %>% 
   group_by(CustomerID) %>% 
-  summarise(Recency = as.numeric(today()- max(InvoiceDate)), 
-            Frequency = n(), 
-            Monetary = sum(Total)) %>% 
-  mutate(R_score = ntile(desc(Recency), 4), 
+  summarise(Monetary = sum(Total), 
+            Frequency = n(),
+            Recency = min(Recency)) %>% 
+  mutate(M_score = ntile(Monetary, 4), 
          F_score = ntile(Frequency, 4),
-         M_score = ntile(Monetary, 4)) %>% 
+         R_score = ntile(desc(Recency), 4)) %>% 
   mutate(classificazione = case_when(R_score == 4 & F_score == 4 & M_score == 4 ~ "Gold", 
                                      R_score == 1 & F_score == 1 & M_score == 1 ~ "Lost", 
-                                     R_score == 4 ~ "High Spending", 
-                                     F_score == 4 ~ "Loyal"))
-
-
-ggplot(rfm) +
-  geom_histogram(aes(Recency, fill=factor(R_score))) +
-  labs(title="Recency and R score")
-
-ggplot(rfm) +
-  geom_histogram(aes(Frequency, fill=factor(F_score))) +
-  labs(title="Frequency and F score")
-
-ggplot(rfm) +
-  geom_histogram(aes(Monetary, fill=factor(M_score))) +
-  labs(title="Monetary and M score")
-
-summary(rfm)
-
-ggplot(rfm) +
-  geom_bar(aes(classificazione, fill=classificazione))
+                                     F_score == 4  ~ "Loyal", 
+                                     R_score == 4 ~ "Attivi",
+                                     M_score == 4 ~ "High spending"))
 
 
 rfm %>% 
-  filter(!is.na(classificazione)) %>% 
-  ggplot() +
-  geom_bar(aes(classificazione, fill=classificazione))
-
-rfm %>% 
-  filter(!is.na(classificazione)) %>% 
   group_by(classificazione) %>% 
-  summarise(Fatturato = sum(Monetary)) %>% 
-  ggplot() +
-  geom_col(aes(classificazione, Fatturato, fill=classificazione))
-
-rfm %>% 
-  filter(!is.na(classificazione)) %>% 
-  group_by(classificazione) %>% 
-  summarise(Fatturato = sum(Monetary)) %>% 
-  ggplot() +
-  geom_col(aes(classificazione, Fatturato, fill=classificazione)) +
-  scale_y_continuous(labels = function(x) format(x, scientific = FALSE))
+  summarise(n=n(), fatturato=sum(Monetary))
---
+++ progetto.md
@@ -0,0 +1,23 @@
+---
+layout: page
+title: Progetto di gruppo
+permalink: /progetto/
+nav_order: 2
+---
+
+# 💻📈💡 Progetto di gruppo
+
+
+È possibile sostenere l’esame del corso Coding e Big Data in due modalità:
+
+- Consigliato: Progetto di gruppo (consegna e presentazione progetto nella settimana dal 11 dicembre al 15 dicembre 2023)
+- Prova individuale della durata di 1h (durante le sessioni ufficiali del corso)
+
+
+## 🗓 Iscrizione
+
+Per iscriversi all’esame nella modalità Progetto di gruppo sarà necessario compilare individualmente al form entro il 3 dicembre 2023. 
+
+[Iscrizione al progetto di gruppo](https://docs.google.com/forms/d/1PZ9SlmDImu8shwokEKXFF4dggTxc83jZdb9J2CqULgk){: .btn .btn-blue }
+
+Successivamente sarà necessario iscriversi ad uno degli appelli delle sessioni ufficiali per poter registrare il voto. 
