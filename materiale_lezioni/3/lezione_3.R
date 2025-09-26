4810ff3f704b00d48c3993889fd94209c2e7c3a2
Vincenzo Nardelli <vincnardelli@gmail.com>
2025-09-26 09:21:27 +0200

    Add lezione 3

---
+++ materiale_lezioni/3/lezione_3.R
@@ -0,0 +1,50 @@
+numero <- 1
+numero = 1
+
+testo <- "Questo è un testo"
+
+nome_cliente <- "Giorgio"
+NomeCliente <- "Antonio"
+nomecliente <- "Luca"
+
+nomecliente1 <- "Roberto"
+
+clienti <- c("Antonio", "Luca", "Giorgio")
+clienti_stringa <- "Antonio, Luca, Giorgio"
+
+clienti
+clienti[3]
+
+eta <- c(23, 19, 93)
+eta[3]
+
+vettore <- c(1, 2, 3)
+1:3
+
+clienti[2:3]
+
+1 == 1
+1 == 2
+18 > 12
+12 < 90
+
+eta < 35
+eta == 19
+
+clienti[c(TRUE, FALSE, TRUE)]
+clienti[c(1, 3)]
+
+clienti[eta < 35]
+
+voto <- 12
+
+if(voto >= 18){
+  status <- "Promosso"
+}else{
+  status <- "Bocciato"
+}
+
+
+
+
+
